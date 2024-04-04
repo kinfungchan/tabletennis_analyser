49d97d63efdebe6693d4cdf6188f5e93c55fe705
kin8chan <kinfung@hotmail.co.uk>
2024-04-04 23:54:33 +0100

    Start of Project with Ultralytics and YOLO

---
+++ yolo_inference.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+
+model = YOLO('yolov8x')
+# model = YOLO('models/last.pt')
+
+# Predicting on an image
+# result = model.predict('input_videos/image.png', save=True)
+
+# Predicting on a video
+result = model.predict('input_videos/input_video.mp4', conf=0.2, save=True)
+
+# Tracking on a video
+# result = model.track('input_videos/input_video.mp4', conf=0.2, save=True)
+
+print(result)
+print("boxes:")
+for box in result[0].boxes:
+    print(box)
+

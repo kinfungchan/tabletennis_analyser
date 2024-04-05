31d3c5f8f1b39d706d90107410106e42d0dc00d1
kin8chan <kinfung@hotmail.co.uk>
2024-04-05 22:58:54 +0100

    Training a Model with ultralytics to get weights, and then using yolov8x for tracking

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,16 +1,18 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8x')
-# model = YOLO('models/last.pt')
+
+# Using Weights from Model training with Roboflow 
+# model = YOLO('models/yolo5_last.pt')
 
 # Predicting on an image
 # result = model.predict('input_videos/image.png', save=True)
 
 # Predicting on a video
-result = model.predict('input_videos/input_video.mp4', conf=0.2, save=True)
+# result = model.predict('input_videos/input_video.mp4', conf=0.2, save=True)
 
 # Tracking on a video
-# result = model.track('input_videos/input_video.mp4', conf=0.2, save=True)
+result = model.track('input_videos/input_video.mp4', conf=0.2, save=True)
 
 print(result)
 print("boxes:")
